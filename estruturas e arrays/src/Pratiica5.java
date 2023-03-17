54856a7bf246a837519dd8ed9f2511716c0f417d
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-03-16 21:14:24 -0400

    atualizando versões

---
+++ estruturas e arrays/src/Pratiica5.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Pratiica5 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Tabuada de: ");
+        int tabuada = scan.nextInt();
+    }
+}
