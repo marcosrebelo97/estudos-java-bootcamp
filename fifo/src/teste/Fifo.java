d35f5ba8568db5c01cadba8b58eda1305e68bdf3
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-03-17 15:18:27 -0400

    implementando fila

---
+++ fifo/src/teste/Fifo.java
@@ -0,0 +1,72 @@
+package teste;
+
+public class Fifo {
+    private No refNoEntradaFila;
+
+    public Fifo() {
+        this.refNoEntradaFila = null;
+    }
+
+    public void enqueue(No novoNo) {
+        novoNo.setRefNo(refNoEntradaFila);
+        refNoEntradaFila = novoNo;
+    }
+
+    public No first() {
+        if (!this.isEmpty()) {
+            No primeiroNo = refNoEntradaFila;
+            while (true) {
+                if (primeiroNo.getRefNo() != null) {
+                    primeiroNo = primeiroNo.getRefNo();
+                } else {
+                    break;
+                }
+            }
+        }
+        return null;
+    }
+
+    public No dequeue() {
+        if (!this.isEmpty()) {
+            No primeiroNo = refNoEntradaFila;
+            No noAuxiliar = refNoEntradaFila;
+            while (true) {
+                if (primeiroNo.getRefNo() != null) {
+                    noAuxiliar = primeiroNo;
+                    primeiroNo = primeiroNo.getRefNo();
+                } else {
+                    noAuxiliar.setRefNo(null);
+                    break;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return refNoEntradaFila == null ? true : false;
+    }
+
+    @Override
+    public String toString() {
+        String retorno = "";
+        No noAuxiliar = refNoEntradaFila;
+
+        if (refNoEntradaFila != null) {
+            while (true) {
+                retorno += "[No{objeto= " + noAuxiliar.getObject() + "}]";
+                if (noAuxiliar.getRefNo() != null) {
+                    noAuxiliar = noAuxiliar.getRefNo();
+                } else {
+                    retorno += "null";
+                    break;
+                }
+            }
+        } else {
+            retorno = null;
+        }
+
+        return retorno;
+    }
+
+}
---
+++ fifo/src/teste/No.java
@@ -0,0 +1,39 @@
+package teste;
+
+public class No {
+
+    private Object object;
+    private No refNo;
+
+    public No() {
+    }
+
+    public No(Object object, No refNo) {
+        this.refNo = null;
+        this.object = object;
+    }
+
+    public Object getObject() {
+        return this.object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+
+    public No getRefNo() {
+        return this.refNo;
+    }
+
+    public void setRefNo(No refNo) {
+        this.refNo = refNo;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " object='" + getObject() + "'" +
+                "}";
+    }
+
+}
