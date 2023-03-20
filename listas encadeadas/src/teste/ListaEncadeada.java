dc5b3516815c8326b228456b701a3ec62935ac02
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-03-20 11:25:38 -0400

    metodos isempty e size

---
+++ listas encadeadas/src/teste/ListaEncadeada.java
@@ -0,0 +1,35 @@
+package teste;
+
+public class ListaEncadeada<T> {
+
+    No<T> referenciaEntrada;
+
+    public ListaEncadeada() {
+        this.referenciaEntrada = null;
+    }
+
+    // metodos
+    public int size() { // tamanho da lista
+        int tamanhoLista = 0;
+        No<T> referenciaAux = referenciaEntrada;
+        while (true) {
+            if (referenciaAux != null) {
+                tamanhoLista++;
+                if (referenciaAux.getProximoNo() != null) {
+                    referenciaAux = referenciaAux.getProximoNo();
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+
+        return tamanhoLista;
+    }
+
+    public boolean isEmpty() {
+        return referenciaEntrada == null ? true : false;
+    }
+
+}
