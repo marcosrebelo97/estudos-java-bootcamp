a0f0effd57d7f43a51efb2944ca061b454381ee0
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-03-20 12:08:09 -0400

    add metodo remove

--- listas encadeadas/src/teste/ListaEncadeada.java
+++ listas encadeadas/src/teste/ListaEncadeada.java
@@ -36,6 +36,23 @@ public class ListaEncadeada<T> {
         return noRetorno;
     }
 
+    public T remove(int index) {
+        No<T> noPivor = this.getNo(index);
+
+        if (index == 0) {
+            referenciaEntrada = noPivor.getProximoNo();
+            return noPivor.getConteudo();
+        }
+
+        No<T> noAnterior = getNo(index - 1);
+        noAnterior.setProximoNo(noPivor.getProximoNo());
+        return noPivor.getConteudo();
+    }
+
+    public T get(int index) {
+        return getNo(index).getConteudo();
+    }
+
     public int size() { // tamanho da lista
         int tamanhoLista = 0;
         No<T> referenciaAux = referenciaEntrada;
