4989bbb8f03aedf3e2ef7301e50d689863fa50bb
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-03-20 12:41:12 -0400

    classe main e testes

--- listas encadeadas/src/teste/ListaEncadeada.java
+++ listas encadeadas/src/teste/ListaEncadeada.java
@@ -29,7 +29,7 @@ public class ListaEncadeada<T> {
         No<T> noAuxiliar = referenciaEntrada;
         No<T> noRetorno = null;
 
-        for (int i = 0; i < this.size() - 1; i++) {
+        for (int i = 0; i <= index; i++) {
             noRetorno = noAuxiliar;
             noAuxiliar = noAuxiliar.getProximoNo();
         }
@@ -94,8 +94,7 @@ public class ListaEncadeada<T> {
         }
 
         strRetorno += null;
-
-        return "[No{conteudo=" + noAuxiliar.getConteudo() + "}] --->";
+        return strRetorno;
 
     }
 
