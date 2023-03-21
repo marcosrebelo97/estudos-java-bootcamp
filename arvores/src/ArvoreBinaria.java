dccf0fdc687b234776279f54dc1c5ac4581c426c
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-03-21 13:57:54 -0400

    metodos de exibição

--- arvores/src/ArvoreBinaria.java
+++ arvores/src/ArvoreBinaria.java
@@ -22,4 +22,44 @@ public class ArvoreBinaria<T extends Comparable<T>> {
         }
         return atual;
     }
+
+    // métodos de exibição
+    public void exibirInOrdem() {
+        System.out.println("\n Exibindo InOrdem");
+        exibirInOrdem(raiz);
+    }
+
+    private void exibirInOrdem(BinNo<T> atual) {
+        if (atual != null) {
+            exibirInOrdem(atual.getNoEsq());
+            System.out.println(atual.getConteudo() + ", ");
+            exibirInOrdem(atual.getNoDir());
+        }
+    }
+
+    public void exibirPosOrdem() {
+        System.out.println("\n Exibindo PosOrdem");
+        exibirPosOrdem(raiz);
+    }
+
+    private void exibirPosOrdem(BinNo<T> atual) {
+        if (atual != null) {
+            exibirPosOrdem(atual.getNoEsq());
+            exibirPosOrdem(atual.getNoDir());
+            System.out.println(atual.getConteudo() + ", ");
+        }
+    }
+
+    public void exibirPreOrdem() {
+        System.out.println("\n Exibindo PreOrdem");
+        exibirPreOrdem(raiz);
+    }
+
+    private void exibirPreOrdem(BinNo<T> atual) {
+        if (atual != null) {
+            System.out.println(atual.getConteudo() + ", ");
+            exibirPreOrdem(atual.getNoEsq());
+            exibirPreOrdem(atual.getNoDir());
+        }
+    }
 }
