61844d367ae8e44a5a8f493528ba4746936a91e9
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-03-21 13:38:10 -0400

    implementação bino

---
+++ arvores/src/BinNo.java
@@ -0,0 +1,47 @@
+public class BinNo<T extends Comparable<T>> {
+
+    private T conteudo;
+    private BinNo<T> noEsq;
+    private BinNo<T> noDir;
+
+    public BinNo(T conteudo) {
+        this.conteudo = conteudo;
+        this.noEsq = this.noDir = null;
+    }
+
+    public BinNo() {
+
+    }
+
+    public T getConteudo() {
+        return this.conteudo;
+    }
+
+    public void setConteudo(T conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public BinNo<T> getNoEsq() {
+        return this.noEsq;
+    }
+
+    public void setNoEsq(BinNo<T> noEsq) {
+        this.noEsq = noEsq;
+    }
+
+    public BinNo<T> getNoDir() {
+        return this.noDir;
+    }
+
+    public void setNoDir(BinNo<T> noDir) {
+        this.noDir = noDir;
+    }
+
+    @Override
+    public String toString() {
+        return "BinNo{" +
+                "conteudo = " + getConteudo() +
+                "}";
+    }
+
+}
