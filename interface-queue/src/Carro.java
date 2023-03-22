b6e5f2fe49ff3a6eff1f55b543fb3796afb18870
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-03-21 20:49:59 -0400

    testando metodos peek, poll, offer

---
+++ interface-queue/src/Carro.java
@@ -0,0 +1,41 @@
+import java.util.Objects;
+
+public class Carro {
+    String marca;
+
+    public Carro(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Carro)) {
+            return false;
+        }
+        Carro carro = (Carro) o;
+        return Objects.equals(marca, carro.marca);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(marca);
+    }
+
+    @Override
+    public String toString() {
+        return "Carro{" +
+                " marca='" + getMarca() + "'" +
+                "}";
+    }
+
+}
---
+++ interface-queue/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Queue<Carro> queueCarros = new LinkedList<>();
+
+        queueCarros.add(new Carro("Ford"));
+        queueCarros.add(new Carro("Chevrolet"));
+        queueCarros.add(new Carro("Fiat"));
+
+        System.out.println(queueCarros.add(new Carro("Peugeout")));
+        System.out.println(queueCarros);
+
+        System.out.println(queueCarros.offer(new Carro("Renault")));
+
+        System.out.println(queueCarros.peek()); // não remove
+        System.out.println(queueCarros);
+
+        System.out.println(queueCarros.poll()); // remove
+        System.out.println(queueCarros);
+    }
+
+}
