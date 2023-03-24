e13ac11a96de786ccca4f33a1e07d9620d87873e
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-03-24 13:12:33 -0400

    atualizando

---
+++ collections/src/ExList.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExList {
+    public static void main(String[] args) throws Exception {
+
+        List<Double> notas = new ArrayList<>();
+
+        notas.add(10.0);
+        notas.add(9.0);
+        notas.add(8.0);
+        notas.add(11.0);
+        notas.add(12.0);
+
+        System.out.println(notas);
+
+    }
+}
