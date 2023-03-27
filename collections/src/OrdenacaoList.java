2897c3de257d639be41f357e53f50cefebb47b12
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-03-27 14:51:29 -0400

    ordenacao

---
+++ collections/src/OrdenacaoList.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoList {
+    public static void main(String[] args) {
+
+        List<Gato> meusGatos = new ArrayList<>() {
+            {
+                add(new Gato("Jon", 18, "preto"));
+                add(new Gato("Simba", 6, "tigrado"));
+                add(new Gato("Jon", 12, "amarelo"));
+            }
+        };
+
+        System.out.println("--\tOrdem de Inserção\t--");
+        System.out.println(meusGatos);
+
+        System.out.println("--\tOrdem Aleatória\t--");
+        Collections.shuffle(meusGatos);
+        System.out.println(meusGatos);
+
+        System.out.println("--\tOrdem Natural\t--");
+        System.out.println(meusGatos);
+
+    }
+}
+
+class Gato {
+    private String nome;
+    private Integer idade;
+    private String cor;
+
+    public Gato(String nome, Integer idade, String cor) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cor = cor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "nome = " + getNome() +
+                ", idade = " + getIdade() +
+                ", cor = " + getCor() +
+                "}";
+    }
+
+}
