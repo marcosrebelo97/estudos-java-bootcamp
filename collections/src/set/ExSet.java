a30676a42624c6e30c819cba6060cffc8d46e43a
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-03-27 16:18:10 -0400

    hashset

--- collections/src/set/ExSet.java
+++ collections/src/set/ExSet.java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 class ExSet {
     public static void main(String[] args) {
+
+        System.out.println("Conjunto de notas: ");
+        Set<Double> notas = new HashSet<>(Arrays.asList(7d, 8.5, 9.3, 7d, 0d, 3.6));
+        System.out.println(notas);
+
+        System.out.println("");
 
     }
 }
