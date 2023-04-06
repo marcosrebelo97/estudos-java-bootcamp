0a7a5e2702049be7890d63a35add42872d021b67
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-04-06 11:17:46 -0400

    before e after

--- junit-exemplos/src/test/ConsultarDadosPessoaTeste.java
+++ junit-exemplos/src/test/ConsultarDadosPessoaTeste.java
@@ -1,5 +1,28 @@
 package test;
 
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.junit.*;
+
+import main.GerenciadorConexaoBD;
+
 public class ConsultarDadosPessoaTeste {
+
+    @Before
+    public void configuraConexao() {
+        GerenciadorConexaoBD.iniciarConexao();
+        System.out.println("Rodou configuraConexao");
+    }
+
+    @Test
+    public void validarDadosRetorno() {
+        assertTrue(true);
+    }
+
+    @After
+    public void finalizarConexao() {
+        GerenciadorConexaoBD.finalizarConexao();
+        System.out.println("Rodou finalizarConexao");
+    }
 
 }
