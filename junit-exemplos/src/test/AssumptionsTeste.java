15f39628cf6de68e0e4303c91a5eb02e424b5b69
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-04-06 12:41:25 -0400

    assumeTrue

--- junit-exemplos/src/test/AssumptionsTeste.java
+++ junit-exemplos/src/test/AssumptionsTeste.java
@@ -1,5 +1,15 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assume.assumeTrue;
+import org.junit.Test;
+
 public class AssumptionsTeste {
+
+    @Test
+    public void validarUsuarioMarcos() {
+        assumeTrue("marcos".equals(System.getenv("USER")));
+        assertEquals(10, 5 + 5);
+    }
 
 }
