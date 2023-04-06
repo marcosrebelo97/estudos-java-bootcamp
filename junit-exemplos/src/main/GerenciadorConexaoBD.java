4d6eb28a4d6bc6a6be1ebc0a68f2ec1a2202a1c8
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-04-06 11:03:58 -0400

    logger test

---
+++ junit-exemplos/src/main/GerenciadorConexaoBD.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.util.logging.*;
+
+public class GerenciadorConexaoBD {
+
+    private static final Logger LOGGER = Logger.getLogger(GerenciadorConexaoBD.class.getName());
+
+    public static void iniciarConexao() {
+        // faz algo
+        LOGGER.info("Inicou a conexao");
+    }
+
+    public static void finalizarConexao() {
+        // faz algo
+        LOGGER.info("Finalizou a conexao");
+    }
+
+}
