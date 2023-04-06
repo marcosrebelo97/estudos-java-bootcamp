ba947b10d0676f81ecff45b8eea58855a34e7092
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-04-06 12:57:29 -0400

    validando excecoes

--- junit-exemplos/src/test/ExceptionsTeste.java
+++ junit-exemplos/src/test/ExceptionsTeste.java
@@ -1,11 +1,21 @@
 package test;
 
+import static org.junit.Assert.assertThrows;
+
 import org.junit.Test;
+
+import main.Conta;
+import main.TransferenciaContas;
 
 public class ExceptionsTeste {
 
     @Test
     public void validarCenarioExcecaoTransferencia() {
+        Conta contaOrigem = new Conta("13212", 0);
+        Conta contaDestino = new Conta("87954", 100);
+
+        assertThrows(IllegalArgumentException.class,
+                () -> transferenciaContas.transfere(contaOrigem, contaDestino, -1));
 
     }
 }
