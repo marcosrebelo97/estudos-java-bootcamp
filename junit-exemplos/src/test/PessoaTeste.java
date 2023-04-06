7abd65ef8d0bb5c6b167480ed40e8c716b5a411c
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-04-06 10:09:02 -0400

    confirmando testes

--- junit-exemplos/src/test/PessoaTeste.java
+++ junit-exemplos/src/test/PessoaTeste.java
@@ -1,18 +1,15 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
-
 import java.time.LocalDateTime;
-
 import org.junit.Test;
-
 import main.Pessoa;
 
 public class PessoaTeste {
 
     @Test
     public void deveCalcularIdadeCorretamente() {
-        Pessoa jessica = new Pessoa("Jéssica", LocalDateTime.of(2000, 1, 1, 15, 1, 1));
+        Pessoa jessica = new Pessoa("Jéssica", LocalDateTime.of(2000, 1, 1, 15, 0, 0));
         assertEquals(23, jessica.getIdade());
     }
 
