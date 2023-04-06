916c483a56d10db82f6d3c48cd8d5a5fb0defa3b
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-04-06 10:25:40 -0400

    exercitando assertEquals, assertArrayList

---
+++ junit-exemplos/src/test/AssertionsTeste.java
@@ -0,0 +1,44 @@
+package test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
+
+import java.time.LocalDateTime;
+
+import org.junit.Test;
+
+import main.Pessoa;
+
+public class AssertionsTeste {
+
+    @Test
+    public void validarLancamento() {
+        int[] primeiroLancamento = { 10, 20, 30, 40, 50 };
+        int[] segundoLancamento = { 10, 20, 30, 40, 50 };
+
+        assertArrayEquals(primeiroLancamento, segundoLancamento);
+
+    }
+
+    @Test
+    public void validarObjetoEstaNulo() {
+        Pessoa pessoa = null;
+        assertNull(pessoa);
+
+        /*
+         * pessoa = new Pessoa("Luciano", LocalDateTime.now());
+         * assertNull(pessoa);
+         */
+    }
+
+    @Test
+    public void validarNumerosDeTiposDiferentes() {
+        double valor = 5.0;
+        double outroValor = 5.0;
+
+        assertEquals(outroValor, valor, outroValor);
+    }
+
+}
--- junit-exemplos/src/test/PessoaTeste.java
+++ junit-exemplos/src/test/PessoaTeste.java
@@ -1,6 +1,8 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.time.LocalDateTime;
 import org.junit.Test;
 import main.Pessoa;
@@ -16,7 +18,9 @@ public class PessoaTeste {
     @Test
     public void deveRetornarSeEhMaiorDeIdade() {
         Pessoa jessica = new Pessoa("Jéssica", LocalDateTime.of(2000, 1, 1, 15, 1, 1));
-        assertEquals(23, jessica.getIdade());
+        assertTrue(jessica.ehMaiorDeIdade());
+
+        // Pessoa joao = new Pessoa("João", LocalDateTime.now());
     }
 
 }
